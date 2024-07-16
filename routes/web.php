fdf9fdaf3e902dbc8837c0d50fb311ed8b13f6f4
unknown <fribyany6661@gmail.com>
2024-07-16 13:03:03 +0700

    membuat dashboard

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/template', function () {
+    return view('template');
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.layouts.wrapper');
 });
 
 Route::get('/hello', function () {
