6a14b82dba1250a5eae83d6d67f0474bfc56d7a2
ahmadnajib18 <ahmadnajibsyafii3@gmail.com>
2024-07-19 19:57:56 +0700

    update page tambah transaksi

--- resources/views/admin/transaksi/create.blade.php
+++ resources/views/admin/transaksi/create.blade.php
@@ -69,58 +69,59 @@
     </div>
 
     <div class="col-md-6">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>No</th>
-                    <th>Nama Produk</th>
-                    <th>Jumlah Produk</th>
-                    <th>#</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Mi Goreng</td>
-                    <td>2</td>
-                    <td>
-                        <a href="#" class="text-danger"><i class="fas fa-times"></i></a> 
-                    </td>
-                </tr>
-                <!-- Tambahkan baris lainnya di sini -->
-            </tbody>
-        </table>
-
-        <a href="#" class="btn btn-success"><i class="fas fa-check"></i> Sukses</a>
-        <a href="#" class="btn btn-warning"><i class="fas fa-hourglass-half"></i> Pending</a>
-    </div>
-</div>
-
-<div class="row p-2">
-    <div class="col-md-6">
-        <div class="car">
-            <div class="card-body">
-
-                <div class="form-group">
-                    <label for="">Total</label>
-                    <input type="number" name="total" class="form-control" id="">
-                </div>
-
-                <div class="form-group">
-                    <label for="">Bayar</label>
-                    <input type="number" name="bayar" class="form-control" id="">
-                </div>
-
-                <button type="submit" class="btn btn-primary">Hitung</button>
-
-                <hr>
-
-                <div class="form-group">
-                    <label for="">Uang Kembalian</label>
-                    <input type="number" disabled name="bayar" class="form-control" id="">
-                </div>
-
-            </div>
+    <div class="card">
+        <div class="card-title p-4">
+            <h4><b>Daftar Produk</b></h4>
+        </div>
+        <div class="card-body">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Nama Produk</th>
+                        <th>Jumlah Produk</th>
+                        <th>#</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Mi Goreng</td>
+                        <td>2</td>
+                        <td>
+                            <a href="#" class="text-danger"><i class="fas fa-times"></i></a>
+                        </td>
+                    </tr>
+                    <!-- Tambahkan baris lainnya di sini -->
+                </tbody>
+            </table>
+            <a href="#" class="btn btn-success"><i class="fas fa-check"></i> Sukses</a>
+            <a href="#" class="btn btn-warning"><i class="fas fa-hourglass-half"></i> Pending</a>
         </div>
     </div>
 </div>
+
+<div class="col-md-6 mb-4">
+            <div class="card">
+                <div class="card-title p-4">
+                    <h4><b>Hitung Kembalian</b></h4>
+                </div>
+                <div class="card-body">
+                    <div class="form-group">
+                        <label for="total">Total</label>
+                        <input type="number" name="total" class="form-control" id="total">
+                    </div>
+                    <div class="form-group">
+                        <label for="bayar">Bayar</label>
+                        <input type="number" name="bayar" class="form-control" id="bayar">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Hitung</button>
+                    <hr>
+                    <div class="form-group">
+                        <label for="kembalian">Uang Kembalian</label>
+                        <input type="number" disabled name="kembalian" class="form-control" id="kembalian">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
